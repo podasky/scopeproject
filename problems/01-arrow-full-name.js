e0e90934e035407400d74bbffe1c1ca3c51e7440
Logan French <Loganfrench04@gmail.com>
2023-08-16 20:06:17 -0400

    Solved 01

--- problems/01-arrow-full-name.js
+++ problems/01-arrow-full-name.js
@@ -13,8 +13,16 @@ let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
 arrowGetFullName(p2); // => 'Charlie Brown'
 
 ***********************************************************************/
+let arrowGetFullName = (person) => {
+  return person.firstName + ' ' + person.lastName
+}
 
-// Your code here
+
+const p1 = { firstName: 'John', lastName: 'Doe' };
+arrowGetFullName(p1); // => 'John Doe'
+const p2 = { firstName: 'Charlie', lastName: 'Brown', age: 9 };
+arrowGetFullName(p2); // => 'Charlie Brown'
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
