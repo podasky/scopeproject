34e356df82fd28a800ba9fead82259d1065c83a4
Logan French <Loganfrench04@gmail.com>
2023-08-17 19:28:25 -0400

    Solved 07

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -18,7 +18,19 @@ funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 ***********************************************************************/
 
-// Your code here
+
+const funcTimer = (time, func) => {
+  setTimeout(() => {
+    console.log("Party time!")
+  }, time);
+}
+
+funcTimer(5000, setTimeout)
+// function partyFunc() {
+//   console.log("Party time!")
+// }
+
+// funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
