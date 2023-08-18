8174bac2704eddaf3a6084450aff55a0b0f17078
Preston <ba6stu13@gmail.com>
2023-08-17 21:22:36 -0400

    Problem 10 done

--- problems/10-count-down.js
+++ problems/10-count-down.js
@@ -30,6 +30,35 @@ Example 4:
 
 // Your code here
 
+function countDownTimer(n){
+    let count = () => {
+        n --;
+        if (n > 0){
+          return count;
+        }
+        else{return "Happy New Year!"}
+    }
+    return count;
+}
+
+//Example 1:
+  console.log(countDownTimer(0)); // prints "Happy New Year!"
+
+//Example 2:
+  let oneDay = countDownTimer(1); // returns a function
+  console.log(oneDay()); // prints "Happy New Year!"
+
+//Example 3:
+  let twoDays = countDownTimer(2); // returns a function
+  console.log(twoDays()); // returns a function
+  console.log(twoDays()); // prints "Happy New Year!"
+
+//Example 4:
+  let threeDays = countDownTimer(3); // returns a function
+  console.log(threeDays()); // returns a function
+  console.log(threeDays()); // returns a function
+  console.log(threeDays()); // prints "Happy New Year!"
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
