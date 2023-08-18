1ee3d1a04f3bf842f2cf9f95ee86d31d9dff8a98
Logan French <Loganfrench04@gmail.com>
2023-08-17 20:00:27 -0400

    Solved 9

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -16,7 +16,15 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 ***********************************************************************/
 
-// Your code here
+const interrupter = (interruptingWord) => {
+  return function (str) {
+    return str.split(' ').join(' ' + interruptingWord + ' ')
+  }
+}
+
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
